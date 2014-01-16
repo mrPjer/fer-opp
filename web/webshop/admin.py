1a65acff56d74a081ca4b2d0c75d31b8ecbd9930
Luka Strižić <luka.strizic@hotmail.com>
2014-01-16 21:17:13 +0000

    git-svn-id: svn://adalgrim.zemris.fer.hr/SilkRoad@33 4a17ba9f-6148-4f47-af36-9530067072c1

--- web/webshop/admin.py
+++ web/webshop/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from webshop.models import *
 
-admin.site.register(ShopInfo)
-admin.site.register(Comment)
+class ShopInfoRegister(admin.ModelAdmin):
+	fields = ['description', 'value']
+
+class CommentRegister(admin.ModelAdmin):
+	fields = ['username','content']
+
+admin.site.register(ShopInfo,ShopInfoRegister)
+admin.site.register(Comment,CommentRegister)
 admin.site.register(Meal)
 admin.site.register(PaymentType)
 admin.site.register(Order)
